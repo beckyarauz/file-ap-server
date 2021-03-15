60da3f2003d0c5c7fe238c2c8ee26419d25de62d
beckyarauz <rebeca.arauz@ondemandcommerce.com>
2021-03-15 11:02:15 +0100

    clean index file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-// import cors from 'cors';
-// import express, { NextFunction, Request, Response } from 'express';
-// import httpStatus from 'http-status';
-// import appRouter from './api/routes';
-// import APIError from './libraries/APIError';
 import { createApi } from './api/app';
 import Config from './config/config';
 import createDB from './config/database';
