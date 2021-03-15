1801dd2d77d4d85de10eae0458eaf63e24d38174
beckyarauz <rebeca.arauz@ondemandcommerce.com>
2021-03-15 09:50:03 +0100

    create api function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import appRouter from './api/routes';
+// import cors from 'cors';
+// import express, { NextFunction, Request, Response } from 'express';
+// import httpStatus from 'http-status';
+// import appRouter from './api/routes';
+// import APIError from './libraries/APIError';
+import { createApi } from './api/app';
 import Config from './config/config';
 import createDB from './config/database';
 import { logger } from './config/logger';
-import APIError from './libraries/APIError';
+
 
 process.on('unhandledRejection', (exception: Error) => {
   logger.error('unhandled-promise-rejection', exception);
@@ -15,31 +17,4 @@ Config.init();
 
 createDB();
 
-const app = express();
-
-app.use(cors());
-
-app.use('/api', appRouter);
-
-app.use((err: Error | APIError, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof APIError) {
-    res
-      .status(err.status)
-      .send(err.message);
-  } else {
-    res
-      .status(httpStatus.INTERNAL_SERVER_ERROR)
-      .send('Something went wrong');
-  }
-  logger.error('api-error', err);
-});
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const err = new APIError('404 Not Found', httpStatus.NOT_FOUND);
-  return next(err);
-});
-
-// start the Express server
-app.listen(process.env.PORT, () => {
-  console.log(`server started at ${process.env.APP_HOST}${process.env.PORT}`);
-});
+createApi();
